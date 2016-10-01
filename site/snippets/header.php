02cdd9122c6b5505ae33e9defe18c95da997a97c
Rafael Polutta <rafael@diccce.com>
2016-10-01 20:12:12 +0200

    Header & Footer
    Looked through the initial project and changed the header and footer
    styles.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -15,8 +15,5 @@
 <body>
 
   <header class="header cf" role="banner">
-    <a class="logo" href="<?php echo url() ?>">
-      <img src="<?php echo url('assets/images/logo.svg') ?>" alt="<?php echo $site->title()->html() ?>" />
-    </a>
     <?php snippet('menu') ?>
   </header>
