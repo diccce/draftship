075567c51ef36e2934d4f0410b1726ca8d1348f2
Rafael Polutta <rafael@diccce.com>
2016-10-01 19:04:51 +0200

    Admin
    Created my admin account and uploaded an avatar.

---
+++ site/accounts/rafael.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: rafael
+password: >
+  $2a$10$MAReLNZfX7yAS.ue5S/.weJfwtpABkUDbx.zM3ewsW6wEuv1OnW6u
+email: rafael@draftship.com
+language: en
+role: admin
+firstname: Rafael
+lastname: Polutta
