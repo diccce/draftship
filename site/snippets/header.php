8fab2d86d5bb0445e117965d11555eab8e438dc0
Rafael Polutta <rafael@diccce.com>
2016-11-02 14:12:10 +0100

    Flexbox Refinment
    Found some cases when it wasn’t shown right.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -9,6 +9,10 @@
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
 
+  <link rel="shortcut icon" href="/assets/images/favicon.png" type="image/png">
+  <link rel="icon" href="/assets/images/favicon.png" type="image/png">
+  <link rel="apple-touch-icon-precomposed" href="/assets/images/apple-touch-icon-precomposed.png">
+
   <?php echo css('assets/css/main.css') ?>
 
 </head>
--- site/snippets/products.php
+++ site/snippets/products.php
@@ -1,18 +1,20 @@
 <div class="flex-grid cf">
-  <?php foreach(page('collection')->children()->children()->visible() as $product): ?>
+  <?php foreach(page('collection')->children()->visible()->children()->visible() as $product): ?>
 
     <div class="col">
-
-      <!--
-      <h3><a href="<?php echo $product->url() ?>"><?php echo $product->title()->html() ?></a></h3>
-      <p><?php echo $product->text()->excerpt(80) ?> <a href="<?php echo $product->url() ?>">read&nbsp;more&nbsp;→</a></p>
-      -->
 
       <?php if($image = $product->images()->sortBy('sort', 'asc')->first()): ?>
       <a href="<?php echo $product->url() ?>">
         <img src="<?php echo $image->url() ?>" alt="<?php echo $product->title()->html() ?>" >
       </a>
       <?php endif ?>
+
+      <span class="col-text">
+      <a href="<?php echo $product->url() ?>">
+        <h3><?php echo $product->title()->html() ?></h3>
+        <p><?php echo $product->design() ?></p>
+      </a>
+      </span>
 
     </div>
 
